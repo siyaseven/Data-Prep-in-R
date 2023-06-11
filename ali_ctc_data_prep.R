68b11283ae9b3cc941598c87c2d49e41d0ec3d40
siyaseven <siyabonga@aims.ac.za>
2023-06-11 17:01:44 +0200

    Using Mutate() function to replace zeros

--- ali_ctc_data_prep.R
+++ ali_ctc_data_prep.R
@@ -8,7 +8,7 @@ library("readxl")# reading data from excel
 library(readr) # reading data from csv 
 library(writexl) # writing to an excel file
 library(dplyr) # Data manipulation
-
+library("ggplot2") #For plotting
 
 #########################################
 # 1. Load Data to R working environment
@@ -20,36 +20,33 @@ FILENAME_DATA = 'Ali_CTC .csv'
 filepath_data <- file.path(DIR_DATA, FILENAME_DATA)
 df_ali_ctc <- readr::read_csv(file = filepath_data)
 
-
 ###################################################
 #.2 Check the glimpse of data.
 ###################################################
-#Viewing and checking the glimpse of the meta data                     
-summary(df_ali_ctc)
+#Viewing and checking the glimpse of the meta data   
+dplyr::glimpse(df_ali_ctc)
 head(df_ali_ctc)
 tail(df_ali_ctc)
 dim(df_ali_ctc)
 colnames(df_ali_ctc)
 ncol(df_ali_ctc)
 nrow(df_ali_ctc)
+
 ##########################################################################
 #.3 Zeros correspond to missing data. Check how many zeros in each column.
 ##########################################################################
-
 colSums(df_ali_ctc==0)
 
 #########################################################################
 #.4 Check the percentage of Zeros entries in each columns.
 # This is checking the % of missing data in the observed data.
 ##########################################################################
-
 colSums((df_ali_ctc==0)/136690)*100
 
 ############################################################################
 #.5 If the Percentage of the missing data is >= 90. This is column is an
 # outlier. Here we get all the outlier columns and write them to excel file,
 ############################################################################
-
 possibleOutliers =  data.frame() #creating an empty df to store columns that will be deleted..
 DF_to_excel = data.frame() #creating an empty df to store the data to be written to excel.
 
@@ -64,7 +61,6 @@ for (i in 1:ncol(df_ali_ctc))
     P = (colSums(df_ali_ctc[,i]==0)/136690)*100
     temp = c(C,P)
     DF_to_excel = rbind(DF_to_excel,temp)
-
   }
 }
 
@@ -79,11 +75,9 @@ head(DF_to_excel)
 write_xlsx(DF_to_excel, 
            "/home/siyabonga/Documents/OneDrive_1_13-05-2023/outliers_from_meta_data.xlsx")
 
-
 ###############################################################################
 #.6 Removing all the outlier columns in a dataset.
 ##############################################################################
-
 df_ali_ctc_copy = df_ali_ctc # Copying data so that we can manipulate the copy.
 df_without_out = data.frame() #empty data frame to store meta data without ouliers.
 
@@ -95,6 +89,7 @@ for (i in 1:nrow(possibleOutliers))
 }
 
 # checkong the glimpse of the data without outlier columns
+dplyr::glimpse(df_without_out)
 dim(df_without_out)
 head(df_without_out)
 tail(df_without_out)
@@ -103,21 +98,31 @@ tail(df_without_out)
 #.7 Replace all the zero entries in data sets with a random number between 
 #1-1000. Data Imputation.
 ##############################################################################
-
+#Copping data without outliers to be used for Data imputation
 df_without_out_copy = df_without_out
-df_ali_ctc_imput = data_frame()
 
-for (i in 1:ncol(df_without_out))
-{
-  if (colSums(df_without_out_copy[,i]==0))
-  {
-    df_without_out_copy[,i] = sample(1:1000, 1) #this code needs to be update.
-                                                # the random number should be applied to each entry, not just 
-                                                #column.
-    df_ali_ctc_imput = df_without_out_copy
-  }
-}
+#Replacing zeros in a data frame with a random number between 1 to 1000.
+df_ali_ctc_imput <- df_without_out_copy %>%
+  mutate_all(~ifelse(. == 0, sample(1:1000, 1, replace = TRUE), .))
 
-head(df_ali_ctc_imput)
-tail(df_ali_ctc_imput)
+#Checking the data 
+dplyr::glimpse(df_ali_ctc_imput)
 
+# Checking the summary of the data after replacing all the zeros with random numbers
+# between 1 to 1000.
+summary <- df_ali_ctc_imput %>% summarize(
+    across(everything(), list(count = n(), mean = mean, median = median,        
+      min = min,max = max)))
+
+##############################################################
+#8.Plotting mz vs Cancer type
+#############################################################
+
+plot <- ggplot(df_ali_ctc_imput, aes(x=`CTC_HB_JPLT17-20_2017.07.31_2019.03.05_sample3.txt`, y =mz))+
+  geom_line()+
+  labs(x="MZ", y="CTC_HB_JPL")
+
+mean(df_ali_ctc_imput$`CTC_HB_JPLT17-18M1_2017.12.06_2019.03.12_sample1.txt`)
+max(df_ali_ctc_imput$`CTC_HB_JPLT17-18M1_2017.12.06_2019.03.12_sample1.txt`)
+min(df_ali_ctc_imput$`CTC_HB_JPLT17-18M1_2017.12.06_2019.03.12_sample1.txt`)
+plot
